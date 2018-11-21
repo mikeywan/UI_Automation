bc6934c8c726bb964791258dbde977db99388ad2
mikeywan <mikey.wan@outlook.com>
2018-11-21 18:30:19 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,6 +2,7 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     suites: {
         smoke: 'tests/smoke/*.js',
+        datadriven: 'tests/datadriven/*.js',
         browserlog: 'tests/browserlogs/*.js',
         regression: 'tests/regression/*.js',
         pageobject: 'tests/pageobject/pageObject.js'
