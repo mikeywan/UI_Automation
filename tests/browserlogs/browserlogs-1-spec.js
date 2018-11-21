7cbfe724eb701259d8a554364973fb24d83d26b4
mikeywan <mikey.wan@outlook.com>
2018-11-21 18:55:27 +0800

    Update browserlogs-1-spec.js

--- tests/browserlogs/browserlogs-1-spec.js
+++ tests/browserlogs/browserlogs-1-spec.js
@@ -1,5 +1,20 @@
 var browserLogs = require('protractor-browser-logs');
- 
+var fs = require('fs');
+
+// abstract writing screen shot to a file
+function writeScreenShot(data, filename) {
+    var stream = fs.createWriteStream(filename);
+
+    stream.write(new Buffer(data, 'base64'));
+    stream.end();
+}
+
+// ...
+
+// within a test:
+browser.takeScreenshot().then(function (png) {
+    writeScreenShot(png, 'exception.png');
+}); 
 describe('Home page:', function () {
  
   var logs;
